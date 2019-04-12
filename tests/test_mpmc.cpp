6631ccef32cb26188d41ffabde03c95768c4d9d3
Richard Maxwell <jodi.the.tigger@gmail.com>
2019-04-13 09:40:41 +1200

    Added testing skeleton/todo.

--- tests/test_mpmc.cpp
+++ tests/test_mpmc.cpp
@@ -1,9 +1,70 @@
 #include <atomic-queues/mpmc.h>
 
-int main(int argument_count, char** arguments)
+#include <initializer_list>
+#include <cstdio>
+#include <functional>
+
+
+int main(int, char**)
 {
-    (void) argument_count;
-    (void) arguments;
+    struct Result
+    {
+        const char* name;
+        const char* error;
+    };
+
+    std::function<Result()> tests[] =
+    {
+        []() -> Result
+        {
+            const char* name = "Null pointers";
+
+
+            return {name, nullptr};
+        }
+        ,
+        []() -> Result
+        {
+            const char* name = "Create/Destroy";
+
+            return {name, nullptr};
+        }
+        ,
+        []() -> Result
+        {
+            const char* name = "Empty";
+
+            return {name, nullptr};
+        }
+        ,
+        []() -> Result
+        {
+            const char* name = "Full";
+
+            return {name, nullptr};
+        }
+        ,
+        []() -> Result
+        {
+            const char* name = "10,000 sums";
+
+            return {name, nullptr};
+        }
+    };
+
+    for(const auto& test : tests)
+    {
+        Result result = test();
+
+        printf
+        (
+            "Test: %-20s: %s%s\n"
+            , result.name
+            , (result.error ? "FAIL: " : "PASS")
+            , (result.error ? result.error : "")
+        );
+    }
+
 
     return 0;
 }
