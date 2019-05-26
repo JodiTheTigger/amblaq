f6d28d45aa48a1c05aae3a70226337b3634edb8f
Richard Maxwell <jodi.the.tigger@gmail.com>
2019-05-27 07:17:28 +1200

    Added tested example code to readme.

--- README.md
+++ README.md
@@ -11,7 +11,86 @@ work by Dmitry Vyukov.
 Example Usage
 -------------
 
-WIP: TODO
+```c
+#include <stdint.h>
+#include <malloc.h>
+
+#define MY_STRUCT_DATA_COUNT 8
+
+typedef struct My_Struct
+{
+    uint64_t tag;
+    uint8_t  data[MY_STRUCT_DATA_COUNT];
+}
+My_Struct;
+
+#define QUEUE_MP   0
+#define QUEUE_MC   0
+#define QUEUE_TYPE My_Struct
+#define QUEUE_IMPLEMENTATION
+#include <amblaq/queues.h>
+
+int example()
+{
+    // create the queue.
+    Queue_Spsc_My_Struct* queue;
+    size_t bytes = 0;
+
+    if (spsc_make_queue_My_Struct(256, NULL, &bytes) != Queue_Result_Ok)
+    {
+        return 1;
+    }
+
+    queue = (Queue_Spsc_My_Struct*) malloc(bytes);
+
+    if (spsc_make_queue_My_Struct(256, queue, &bytes) != Queue_Result_Ok)
+    {
+        free(queue);
+        return 1;
+    }
+
+    // Queue
+    My_Struct reference =
+    {
+          22
+        , {1,2,3,4,5,6,7,8}
+    };
+
+    if (spsc_enqueue_My_Struct(queue, &reference) != Queue_Result_Ok)
+    {
+        free(queue);
+        return 1;
+    }
+
+    // de-queue
+    My_Struct result;
+
+    if (spsc_dequeue_My_Struct(queue, &result) != Queue_Result_Ok)
+    {
+        free(queue);
+        return 1;
+    }
+
+    // It's the same right?
+    if (result.tag != reference.tag)
+    {
+        free(queue);
+        return 2;
+    }
+
+    for (size_t i = 0; i < MY_STRUCT_DATA_COUNT; i++)
+    {
+        if (result.data[i] != reference.data[i])
+        {
+            free(queue);
+            return 2;
+        }
+    }
+
+    free(queue);
+    return 0;
+}
+```
 
 Status
 ------
--- tests/test_queues.c
+++ tests/test_queues.c
@@ -1,3 +1,87 @@
+// -----------------------------------------------------------------------------
+// Example code for readme.
+// -----------------------------------------------------------------------------
+#include <stdint.h>
+#include <malloc.h>
+
+#define MY_STRUCT_DATA_COUNT 8
+
+typedef struct My_Struct
+{
+    uint64_t tag;
+    uint8_t  data[MY_STRUCT_DATA_COUNT];
+}
+My_Struct;
+
+#define QUEUE_MP   0
+#define QUEUE_MC   0
+#define QUEUE_TYPE My_Struct
+#define QUEUE_IMPLEMENTATION
+#include <amblaq/queues.h>
+
+int example()
+{
+    // create the queue.
+    Queue_Spsc_My_Struct* queue;
+    size_t bytes = 0;
+
+    if (spsc_make_queue_My_Struct(256, NULL, &bytes) != Queue_Result_Ok)
+    {
+        return 1;
+    }
+
+    queue = (Queue_Spsc_My_Struct*) malloc(bytes);
+
+    if (spsc_make_queue_My_Struct(256, queue, &bytes) != Queue_Result_Ok)
+    {
+        free(queue);
+        return 1;
+    }
+
+    // Queue
+    My_Struct reference =
+    {
+          22
+        , {1,2,3,4,5,6,7,8}
+    };
+
+    if (spsc_enqueue_My_Struct(queue, &reference) != Queue_Result_Ok)
+    {
+        free(queue);
+        return 1;
+    }
+
+    // de-queue
+    My_Struct result;
+
+    if (spsc_dequeue_My_Struct(queue, &result) != Queue_Result_Ok)
+    {
+        free(queue);
+        return 1;
+    }
+
+    // It's the same right?
+    if (result.tag != reference.tag)
+    {
+        free(queue);
+        return 2;
+    }
+
+    for (size_t i = 0; i < MY_STRUCT_DATA_COUNT; i++)
+    {
+        if (result.data[i] != reference.data[i])
+        {
+            free(queue);
+            return 2;
+        }
+    }
+
+    free(queue);
+    return 0;
+}
+
+// -----------------------------------------------------------------------------
+
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -496,6 +580,28 @@ int main(int arg_count, char** args)
 {
     (void) arg_count;
     (void) args;
+
+    // -------------------------------------------------------------------------
+    // test the example first
+    // -------------------------------------------------------------------------
+    {
+        int result = example();
+
+        printf
+        (
+              "Test: %s: %-20s: %s%s\n"
+            , "Spsc"
+            , "example"
+            , ((result != 0) ? "FAIL: " : "PASS")
+            , ((result != 0) ? "Bug" : "")
+        );
+
+        if (result != 0)
+        {
+            return 1;
+        }
+    }
+    // -------------------------------------------------------------------------
 
     struct
     {
